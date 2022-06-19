725de670e4360e69ef3ed5ec3155d34cc54c7083
luke <lc169782@protonmail.com>
2022-06-19 10:12:04 +0100

    Currently working with pagy, now moving onto range slider feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   before_action :set_freelancer, only: %i[ show edit update destroy ]
 
 
+
   def index
     @pagy, @freelancers = pagy(Freelancer.order(featured: :desc))
   end
--- app/models/freelancer.rb
+++ app/models/freelancer.rb
@@ -1,3 +1,2 @@
 class Freelancer < ApplicationRecord
-    scope :filter_by_price, -> (cost) { where cost: cost }
 end
